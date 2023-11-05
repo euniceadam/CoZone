b3664b21fd9902bc8184fc0f2b03dd7626caf54b
Eunice Adam <125110475+euniceadam@users.noreply.github.com>
2023-11-05 08:05:14 -0500

    Update Q3.swift

--- Cozone/Views/Q3.swift
+++ Cozone/Views/Q3.swift
@@ -14,41 +14,53 @@ struct Q3: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("What's your")
-                .font(.largeTitle)
+                .font(.custom("Outfit-Regular", size: 40))
                 .padding(.horizontal)
+            
             Text("CoZonality?")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+                .font(.custom("Outfit-SemiBold", size: 40))
                 .foregroundColor(buttonColor)
                 .padding(.leading)
+                .frame(maxWidth: 350, alignment: .leading)
+            
             Text("Answer a few questions to find out what kind of partner you are")
-                .fontWeight(.light)
-                .padding(.leading)
-            Spacer(minLength: 100)
+                .font(.custom("Montserrat-Light", size: 16))
+                .frame(maxWidth: 350, alignment: .leading)
+                .padding()
+            
+            Spacer(minLength: 50)
             
             Text("You find reading:")
-                .font(.title2)
-                .padding(.leading)
+                .font(.custom("Outfit-Medium", size: 26))
+                .frame(maxWidth: 350, alignment: .leading)
+                .padding(.top, -70.0)
+                .padding()
+            
             Button (action: {self.selectedAnswer = "a"}){
                 Text("a. Takes too long; you get fidgety")
                     .padding()
                     .foregroundColor(Color.black)
-                    .background(selectedAnswer == "a" ? Color("lavenderlite") : Color.gray) // Change the button color conditionally
+                    .background(selectedAnswer == "a" ? Color("lavenderlite") : Color.clear)
                     .cornerRadius(10)
+                    .frame(width:300)
             }
+            
             Button (action: {self.selectedAnswer = "b"}){
                 Text("b. The best and most relaxing thing ever")
                     .padding()
                     .foregroundColor(Color.black)
-                    .background(selectedAnswer == "b" ? Color("lavenderlite") : Color.gray) // Change the button color conditionally
+                    .background(selectedAnswer == "b" ? Color("lavenderlite") : Color.clear) // Change the button color conditionally
                     .cornerRadius(10)
+                    .frame(width:300)
             }
+            
             Button (action: {self.selectedAnswer = "c"}){
                 Text("c. Pretty boring; you'd rather be outside")
                     .padding()
                     .foregroundColor(Color.black)
-                    .background(selectedAnswer == "c" ? Color("lavenderlite") : Color.gray) // Change the button color conditionally
+                    .background(selectedAnswer == "c" ? Color("lavenderlite") : Color.clear) // Change the button color conditionally
                     .cornerRadius(10)
+                    .frame(width:300)
             }
             Spacer()
             NavigationLink(destination: Q4()){
@@ -59,7 +71,7 @@ struct Q3: View {
                     .background(selectedAnswer != nil ? buttonColor : Color("lavenderlite"))
                     .cornerRadius(15)
             }
-            .padding()
+            .padding(.leading, 40.0)
             .disabled(selectedAnswer == nil) 
         }
     }
