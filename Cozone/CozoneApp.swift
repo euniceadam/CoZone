93f4a15965425a16e5a1371ce7188519ae43c943
Eunice Adam <adameunice@gmail.com>
2023-11-05 01:56:25 -0500

    Initial Commit

---
+++ Cozone/CozoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozoneApp.swift
+//  Cozone
+//
+//  Created by Eunice Adam on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CozoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
